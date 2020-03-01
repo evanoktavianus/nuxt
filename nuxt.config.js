a4ba884f4463c08438b939fc3a9e62eb0c09ffcb
Litera Institute <literakmsd@gmail.com>
2020-03-01 18:34:41 +0700

    open server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 
 export default {
-  mode: 'universal',
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
+ mode: 'universal',
   /*
   ** Headers of the page
   */
